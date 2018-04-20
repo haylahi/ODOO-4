bec960e9b2ac543ef2bb0c13b1ee0b6f44f84d59
Hariprasath <31165531+hari4274@users.noreply.github.com>
2018-04-20 13:12:28 +0530

    Create exceldatevalue2normaldate.py

---
+++ exceldatevalue2normaldate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+# refference link: https://stackoverflow.com/questions/31359150/convert-date-from-excel-in-number-format-to-date-format-python
+def exceldate2normal(self, exceldate):
+    # example : 43211 to datetime.datetime(2018, 4, 21, 0, 0)
+    if exceldate:
+        return datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(exceldate) - 2)
