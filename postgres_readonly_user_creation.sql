5d61b7dc1a55ad45797a4ea458768b63a35cba2c
Hariprasath <31165531+hari4274@users.noreply.github.com>
2018-07-09 09:19:22 +0530

    Create postgres_readonly_user_creation.sql

---
+++ postgres_readonly_user_creation.sql
@@ -0,0 +1,14 @@
+--Creating a readonly user for accessing tables(No-write permission)
+CREATE ROLE readonlyuser WITH LOGIN PASSWORD 'readonlyuser' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+
+--Assign permission to this read only user:
+GRANT CONNECT ON DATABASE db_name TO readonlyuser;
+\c db_name
+GRANT USAGE ON SCHEMA public TO readonlyuser;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonlyuser;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonlyuser;
+
+--Drop role
+revoke ALL on all tables in schema public from readonlyuser;
+drop owned by readonlyuser;
+drop role readonlyuser;
